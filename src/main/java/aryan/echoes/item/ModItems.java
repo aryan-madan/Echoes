84de0283287c194ae9b7d11bf4a186b9b8825738
trenai <aryan.madan.2022@gmail.com>
2025-09-15 17:46:04 +0530

    Added echoic pearls, and the Primordial Echo.

--- src/main/java/aryan/echoes/item/ModItems.java
+++ src/main/java/aryan/echoes/item/ModItems.java
@@ -14,11 +14,20 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 
 import java.util.function.Function;
 
 public class ModItems {
 
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Echoes.MOD_ID, name));
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+        Registry.register(Registries.ITEM, itemKey, item);
+        return item;
+    }
+
+    // CHORUS ITEMS //
     public static final TagKey<Item> REPAIRS_CHORUS = TagKey.of(Registries.ITEM.getKey(), Identifier.of(Echoes.MOD_ID, "repairs_chorus"));
 
     public static final ToolMaterial CHORUS_TOOL_MATERIAL = new ToolMaterial(
@@ -29,13 +38,6 @@ public class ModItems {
             22,
             REPAIRS_CHORUS
     );
-
-    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
-        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Echoes.MOD_ID, name));
-        Item item = itemFactory.apply(settings.registryKey(itemKey));
-        Registry.register(Registries.ITEM, itemKey, item);
-        return item;
-    }
 
     public static final Item CHORUS_SWORD = register(
             "chorus_sword",
@@ -61,11 +63,34 @@ public class ModItems {
             setting -> new ShovelItem(CHORUS_TOOL_MATERIAL, 1.5f, -3f, setting),
             new Item.Settings());
 
+    // ECHOIC ITEMS
+
+    public static final TagKey<Item> REPAIRS_ECHO = TagKey.of(Registries.ITEM.getKey(), Identifier.of(Echoes.MOD_ID, "repairs_echo"));
+
+    public static final ToolMaterial ECHO_MATERIAL = new ToolMaterial(
+            BlockTags.INCORRECT_FOR_DIAMOND_TOOL,
+            1927,
+            9.0F,
+            3.5F,
+            15,
+            REPAIRS_ECHO
+    );
+
+    public static final Item ECHOIC_PEARL = register("echoic_pearl",
+            Item::new,
+            new Item.Settings().rarity(Rarity.UNCOMMON));
+
+    public static final Item PRIMORDIAL_ECHO = register(
+            "primordial_echo",
+            Item::new,
+            new Item.Settings().rarity(Rarity.EPIC).sword(ECHO_MATERIAL, 4.5f, -2.2f)
+    );
 
     public static void initialize() {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
             content.add(CHORUS_SWORD);
+            content.add(PRIMORDIAL_ECHO);
         });
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content -> {
@@ -73,6 +98,11 @@ public class ModItems {
             content.add(CHORUS_PICKAXE);
             content.add(CHORUS_SHOVEL);
             content.add(CHORUS_HOE);
+            content.add(ECHOIC_PEARL);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
+            content.add(ECHOIC_PEARL);
         });
 
     }
